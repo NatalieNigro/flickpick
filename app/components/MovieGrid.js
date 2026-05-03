e4a4927a2cd74514d4269a8a6323c8723d44e0a7
NatalieNigro <natalie@nigroTC.com>
2026-05-03 14:05:37 -0600

    Create MovieGrid.js

---
+++ app/components/MovieGrid.js
@@ -0,0 +1,29 @@
+"use client";
+
+import MovieCard from "./MovieCard";
+
+export default function MovieGrid({ movies, memory, setMovieStatus }) {
+  if (movies.length === 0) {
+    return null;
+  }
+
+  return (
+    <div
+      style={{
+        marginTop: "24px",
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(260px, 1fr))",
+        gap: "22px",
+      }}
+    >
+      {movies.map((movie, index) => (
+        <MovieCard
+          key={`${movie.title}-${movie.year}-${index}`}
+          movie={movie}
+          memory={memory}
+          setMovieStatus={setMovieStatus}
+        />
+      ))}
+    </div>
+  );
+}
